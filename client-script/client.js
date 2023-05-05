539501d084206ca455d3c050bd7b6d764ebb8be3
bisolao <131967080+bisolao@users.noreply.github.com>
2023-05-05 22:42:27 +0100

    Update client.js
    request updates with code changes

--- client-script/client.js
+++ client-script/client.js
@@ -1,55 +1,49 @@
-const express = require('express');
 const axios = require('axios');
-const jwt = require('jsonwebtoken');
 
-const app = express();
+// Your Auth0 domain
+const AUTH0_DOMAIN = 'https://dev-lmxpjg0gz1s31pr2.us.auth0.com';
 
-const PORT = process.env.PORT || 3000;
-const AUTH0_DOMAIN = 'your-auth0-domain';
-const AUTH0_AUDIENCE = 'your-auth0-audience';
-const AUTH0_CLIENT_ID = 'your-auth0-client-id';
-const S3_BUCKET_NAME = 'your-s3-bucket-name';
-const API_GATEWAY_URL = 'your-api-gateway-url';
-const LAMBDA_FUNCTION_ARN = 'your-lambda-function-arn';
-const CLIENT_EMAIL = 'client-email';
-const CLIENT_PASSWORD = 'client-password';
+// Your API Gateway URL
+const API_GATEWAY_URL = 'https://fu5pyc9kee.execute-api.eu-north-1.amazonaws.com/s3bucket-apistagetest';
 
-app.get('/', async (req, res) => {
+// Your S3 resource key
+const S3_RESOURCE_KEY = 'bucket_test.json';
+
+async function getToken() {
   try {
-    // Authenticate with Auth0
-    const { data: { access_token } } = await axios.post(`https://${AUTH0_DOMAIN}/oauth/token`, {
-      grant_type: 'password',
-      username: CLIENT_EMAIL,
-      password: CLIENT_PASSWORD,
-      audience: AUTH0_AUDIENCE,
-      client_id: AUTH0_CLIENT_ID
+    const response = await axios.post(`${AUTH0_DOMAIN}/oauth/token`, {
+      grant_type: 'client_credentials',
+      username: 'oyewolebisola50@gmail.com',
+      password: 'oyewolebisola50@gmail.com',
+      audience: API_GATEWAY_URL,
+      scope: 'openid profile email',
+      client_id: 'w6zH3l6LBPBVDD3r2vMX7DYAsCXLAGMi',
+      client_secret: 'y605aOU_NZJPtsww3yYu27J-VWusRg3FIBKtbFFQ6oX6x3DXofonHz9NwuTskLEw',
     });
-
-    // Verify the token with the Lambda function
-    const { data: { isAuthorized } } = await axios.post(API_GATEWAY_URL, {
-      token: access_token,
-      bucketName: S3_BUCKET_NAME,
-      lambdaFunctionArn: LAMBDA_FUNCTION_ARN
-    });
-
-    // If authorized, download the file from S3
-    if (isAuthorized) {
-      const { data } = await axios.get(`https://${S3_BUCKET_NAME}.s3.amazonaws.com/my-resource.txt`, {
-        headers: {
-          Authorization: `Bearer ${access_token}`
-        }
-      });
-
-      res.send(data);
-    } else {
-      res.status(401).send('Unauthorized');
-    }
+    return response.data.access_token;
   } catch (error) {
     console.error(error);
-    res.status(500).send('Internal Server Error');
   }
-});
+}
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+async function getS3Resource(token) {
+  try {
+    const response = await axios.get(`${API_GATEWAY_URL}/${S3_RESOURCE_KEY}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      responseType: 'arraybuffer',
+    });
+    console.log(`Retrieved S3 resource with status code ${response.status}`);
+    // TODO: Handle the retrieved resource as needed
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function main() {
+  const token = await getToken();
+  await getS3Resource(token);
+}
+
+main();
